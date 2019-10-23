61a8bf8d4a97110686b64eb3000256c5f0a3b33b
Didi <ofisser86@gmail.com>
2019-10-23 17:42:32 +0300

    add templates

--- try_django/views.py
+++ try_django/views.py
@@ -1,16 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("Hello Django")
+    title = "Hello Django"
+    return render(request, 'index.html', {'title': title})
 
 
 def contact_page(request):
-    return HttpResponse("Contact")
+    return render(request, 'index.html', {'title': "Contact page"})
 
 
 def about_page(request):
-    return HttpResponse("<h1>About page</h1>")
+    return render(request, 'index.html', {"title": "About page"})
 
 
 
