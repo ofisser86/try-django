0fb0cdd874e2130f704026f8180bb70535afddc3
Didi <ofisser86@gmail.com>
2019-10-29 11:29:42 +0200

    Create Django custom form validation

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from .models import BlogPost
 
+
 class BlogPostForm(forms.Form):
     title = forms.CharField()
     slug = forms.SlugField()
@@ -12,3 +13,11 @@ class BlogPostModelForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ['title', 'slug', 'content']
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        qs = BlogPost.objects.filter(title__iexact=title)  # use iexact for key sensitive (upper/lower case no matter)
+        if qs.exists():
+            raise forms.ValidationError("This not unique ")
+        print(title)
+        return title
--- try_django/forms.py
+++ try_django/forms.py
@@ -5,3 +5,10 @@ class ContactForm(forms.Form):
     full_name = forms.CharField()
     email = forms.EmailField()
     content = forms.CharField(widget=forms.Textarea)
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        if email.endswith('.ru'):
+            raise forms.ValidationError("This is not a valid email. Please do not use 'ru' ")
+        print(email)
+        return email
