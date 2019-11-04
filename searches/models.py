bb74d99f256bd5aa2de4f1062fdab6edf634574a
Didi <ofisser86@gmail.com>
2019-11-04 18:39:06 +0200

    Create search model

--- searches/models.py
+++ searches/models.py
@@ -1,3 +1,8 @@
+from django.conf import  settings
 from django.db import models
 
 # Create your models here.
+class SearchQuery(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    query = models.CharField(max_length=220)
+    timestamp = models.DateTimeField(auto_now_add=True)
